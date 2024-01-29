e55caabec9c2a560dbcc8ec0d5542e8ce8293315
Rafael <rafaelferreirab2@gmail.com>
2024-01-29 13:29:25 -0300

    feat: testing multiples branches

--- src/app/(products)/products/edit/[id]/page.tsx
+++ src/app/(products)/products/edit/[id]/page.tsx
@@ -2,8 +2,9 @@
 
 import { useRouter } from 'next/navigation'
 
+import { Alert, AlertDescription, AlertTitle } from '@/src/components/ui/alert'
 import { Button } from '@/src/components/ui/button'
-import { ChevronLeft } from 'lucide-react'
+import { ChevronLeft, Terminal } from 'lucide-react'
 
 type PageParams = {
   params: {
@@ -15,13 +16,26 @@ export default function EditProductPage({ params }: PageParams) {
   const router = useRouter()
 
   return (
-    <div className="flex items-center gap-4 p-4">
-      <h1>product: {params.id}</h1>
+    <div className="flex flex-col items-center gap-8 p-4">
+      <div className="flex items-center gap-4">
+        <h1>product: {params.id}</h1>
 
-      <Button variant="link" onClick={() => router.back()}>
-        <ChevronLeft size={16} />
-        voltar
-      </Button>
+        <Button variant="link" onClick={() => router.back()}>
+          <ChevronLeft size={16} />
+          voltar
+        </Button>
+      </div>
+
+      <section>
+        <Alert className="self-start">
+          <Terminal className="h-4 w-4" />
+          <AlertTitle>este é um teste!</AlertTitle>
+          <AlertDescription>
+            lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci,
+            eligendi!
+          </AlertDescription>
+        </Alert>
+      </section>
     </div>
   )
 }
