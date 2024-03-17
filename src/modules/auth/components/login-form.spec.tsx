a2ce4fd7679c1b1244219318805b42b25b65dbc5
Rafael Bernardino <rafaelferreirab2@gmail.com>
2024-03-17 18:58:58 -0300

    test: add provisory user event test

--- src/modules/auth/components/login-form.spec.tsx
+++ src/modules/auth/components/login-form.spec.tsx
@@ -1,6 +1,5 @@
 import { render, RenderResult } from '@testing-library/react'
-
-// import userEvent, { type UserEvent } from '@testing-library/user-event'
+import userEvent, { type UserEvent } from '@testing-library/user-event'
 
 import { LoginForm } from './login-form'
 
@@ -9,16 +8,27 @@ import { LoginForm } from './login-form'
  */
 describe('login form unit test', async () => {
 	let rendered: RenderResult
-	// let user: UserEvent
+	let user: UserEvent
 
 	beforeEach(() => {
 		rendered = render(<LoginForm />)
-		// user = userEvent.setup()
+		user = userEvent.setup()
 	})
 
 	it('should render the component successfully', async () => {
 		expect(rendered.getByLabelText('E-mail')).toBeInTheDocument()
 		expect(rendered.getByLabelText('Senha')).toBeInTheDocument()
 		expect(rendered.getByRole('button', { name: /entrar/i })).toBeInTheDocument()
+	})
+
+	it('should show the form errors validatons', async () => {
+		await user.type(rendered.getByLabelText('E-mail'), 'john_doe@example.com')
+		await user.type(rendered.getByLabelText('Senha'), '123456')
+		await user.click(rendered.getByRole('button', { name: /entrar/i }))
+
+		/**
+		 * FIXME: fix the mock fn
+		 */
+		expect(vi.fn()).not.toBeCalled()
 	})
 })
