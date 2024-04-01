595256c39ed79436b9b026b492e2d485b9aad55e
Rafael Bernardino <rafaelferreirab2@gmail.com>
2024-04-01 18:29:30 -0300

    feat: add destructive log out button

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -13,36 +13,36 @@ import { ToggleSidebarButton } from "./components/toggle-sidebar-button"
  * IMPORTANT: sidebar icons must be imported from the iconsax package
  */
 const routes = [
-	{ id: 1, label: "Dashboard", icon: Activity, href: "/dashboard" },
-	{ id: 2, label: "Preferências", icon: ProfileCircle, href: "/profile" },
+  { id: 1, label: "Dashboard", icon: Activity, href: "/dashboard" },
+  { id: 2, label: "Preferências", icon: ProfileCircle, href: "/profile" },
 ]
 
 export function Sidebar() {
-	const sidebar = useSidebar((store) => ({ expanded: store.expanded }))
+  const sidebar = useSidebar((store) => ({ expanded: store.expanded }))
 
-	const router = useRouterState()
-	const pathname = router.location.pathname
+  const router = useRouterState()
+  const pathname = router.location.pathname
 
-	return (
-		<aside
-			className={cn(
-				"ease flex h-full w-full max-w-[310px] flex-col py-4 pl-2 duration-300",
-				{ "max-w-[80px]": !sidebar.expanded },
-			)}
-		>
-			<div className="relative flex items-center justify-between">
-				<span className="pl-3 text-4xl font-extrabold tracking-tighter">kn.</span>
-				<ToggleSidebarButton />
-			</div>
-			<Separator className="my-4" />
+  return (
+    <aside
+      className={cn(
+        "ease flex h-full w-full max-w-[310px] flex-col py-4 pl-2 duration-300",
+        { "max-w-[80px]": !sidebar.expanded },
+      )}
+    >
+      <div className="relative flex items-center justify-between">
+        <span className="pl-3 text-4xl font-extrabold tracking-tighter">kn.</span>
+        <ToggleSidebarButton />
+      </div>
+      <Separator className="my-4" />
 
-			<div className="space-y-2">
-				{routes.map(({ id, ...route }) => (
-					<SidebarItem key={id} active={pathname === route.href} {...route} />
-				))}
-			</div>
+      <div className="space-y-2">
+        {routes.map(({ id, ...route }) => (
+          <SidebarItem key={id} active={pathname === route.href} {...route} />
+        ))}
+      </div>
 
-			<ProfileButton />
-		</aside>
-	)
+      <ProfileButton />
+    </aside>
+  )
 }
